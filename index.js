f4ec2b51aebe98b72697193d5400722ed520453a
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2022-03-24 23:35:52 -0400

    Add random stars to background

--- index.js
+++ index.js
@@ -16,3 +16,40 @@ const filterOptions = filterList[0].querySelectorAll("a");
 filterOptions.forEach((filterOption) => {
   filterOption.addEventListener("click", filterProjects);
 });
+
+// Add stars
+const createStars = () => {
+  for (let i = 0; i < 30; i++) {
+    /*
+    left: 280px;
+    right: 280px;
+    top: 200px;
+    bottom: 40px;
+    */
+    // if I don't want stars behind text, subtract element's height and width from areas used for randomness
+
+    const colors = ["pink", "red", "yellow", "green", "blue"];
+
+    let star = document.createElement("div");
+    star.innerHTML = "*";
+
+    // get random color
+    let randomNum = Math.floor(Math.random() * colors.length);
+    let color = colors[randomNum];
+    console.log("color chosen: ", color);
+    star.setAttribute("class", `${color} star`);
+
+    // make position random
+    let browserWidth = document.documentElement.clientWidth;
+    let browserHeight = document.documentElement.scrollHeight;
+    let xPos = Math.floor(Math.random() * (browserWidth - 10) + 10);
+    let yPos = Math.floor(Math.random() * (browserHeight - 10) + 10);
+
+    star.style.left = `${xPos}px`;
+    star.style.top = `${yPos}px`;
+
+    document.body.appendChild(star);
+  }
+};
+
+window.onload = createStars;
