ff87fe21bf714600fb6610c27a362d55742eef20
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2022-04-02 12:32:46 -0400

    Scroll to top with link click

---
+++ project.js
@@ -0,0 +1,8 @@
+// TODO
+// Add project scroll to top
+
+const scrollToTop = () => {
+  // Scroll to top
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
