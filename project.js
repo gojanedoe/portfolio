b9ef903687342f42466f3e7e165cf25a4cd30c16
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2022-04-03 16:51:35 -0400

    Add open and close of image modal/lightbox

--- project.js
+++ project.js
@@ -1,8 +1,32 @@
 // TODO
-// Add project scroll to top
+// Add modal handling
 
 const scrollToTop = () => {
   // Scroll to top
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
+
+//When image is clicked, open modal
+const openModal = () => {
+  document.body.style.overflow = "hidden";
+  modal.style.display = "initial"; // initial is block
+};
+
+//When x is clicked, close modal
+const closeModal = () => {
+  document.body.style.overflow = "auto";
+  modal.style.display = "none";
+};
+
+let modal = document.getElementsByClassName("modal")[0];
+
+// Add clickable event to every "zoomable" image
+let zoomableImages = document.getElementsByClassName("zoomable");
+Object.values(zoomableImages).forEach((image) => {
+  image.addEventListener("click", openModal);
+});
+
+// Add clickable event to close
+let close = document.getElementsByClassName("close");
+close[0].addEventListener("click", closeModal);
