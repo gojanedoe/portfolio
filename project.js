7ff79c1beaa6801deb58ea12d3997f712b9e9abc
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2022-04-04 11:16:17 -0400

    Escape from lightbox with key press

--- project.js
+++ project.js
@@ -1,32 +1,47 @@
-// TODO
-// Add modal handling
-
 const scrollToTop = () => {
-  // Scroll to top
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
 
-//When image is clicked, open modal
-const openModal = () => {
-  document.body.style.overflow = "hidden";
-  modal.style.display = "flex"; //"initial"; // initial is block
+const keyboardHandler = (event) => {
+  event = event || window.event; // if e is undefined, set to window.event
+  console.log("keyboard triggered");
+
+  // only if modal is open
+  if (event.key == "Escape") {
+    console.log(event.key);
+    closeModal();
+  } else if (event.key == "ArrowRight") {
+    // right arrow
+  } else if (event.key == "ArrowLeft") {
+    // left arrow
+  }
 };
 
-//When x is clicked, close modal
+const openModal = () => {
+  document.body.style.overflow = "hidden"; // prevents from background scrolling
+  // overscroll-behavior: contain may be another option
+  modal.style.display = "flex";
+
+  // For accessibility & keyboard access
+  modal.focus();
+};
+
 const closeModal = () => {
   document.body.style.overflow = "auto";
   modal.style.display = "none";
+
+  //TODO: remove/move focus
 };
 
+// Event Listeners
 let modal = document.getElementsByClassName("modal")[0];
+modal.addEventListener("keydown", keyboardHandler);
 
-// Add clickable event to every "zoomable" image
 let zoomableImages = document.getElementsByClassName("zoomable");
 Object.values(zoomableImages).forEach((image) => {
   image.addEventListener("click", openModal);
 });
 
-// Add clickable event to close
 let close = document.getElementsByClassName("close");
 close[0].addEventListener("click", closeModal);
