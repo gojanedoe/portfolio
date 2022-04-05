ca7781a4a306629cefa118447761118c1c132c3b
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2022-04-04 20:36:52 -0400

    Add comments

--- project.js
+++ project.js
@@ -1,3 +1,20 @@
+// TODO
+// Accessibility
+// Aria hidden for modal start
+// Toggle aria hidden for modal
+// Save last focused item (button clicked that opened modal? event in openmodal?)
+// Toggle focus on close
+// -1 tab index for all previous focusable elements
+// Check keyboard focus for all lightbox event triggers (thumbnails)
+// Add aria types to all images
+// Add note about controlling the lightbox (arrow keys and escape)
+// Add labels to all images
+// Thumbnail Alt = View full size version of my screenshot of _____
+// Lightbox Alt = Fuller description of image
+// Meaningful link text / alt for arrow buttons
+
+// Popup/warning if JS not available
+
 const scrollToTop = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
@@ -33,6 +50,8 @@ const closeModal = () => {
 
   //TODO: remove/move focus
 };
+
+// let mainPage = document.getElementById("main-page");
 
 // Event Listeners
 let modal = document.getElementsByClassName("modal")[0];
