d6639811607cab50ad75a82a0c3b28305939903d
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2022-04-11 15:05:37 -0400

    Add change slide on button and key click

--- project.js
+++ project.js
@@ -25,31 +25,32 @@ const scrollToTop = () => {
 const keyboardHandler = (event) => {
   event = event || window.event; // if e is undefined, set to window.event
   console.log("keyboard triggered");
+  console.log(event.key);
 
   // only if modal is open
   if (event.key == "Escape") {
-    console.log(event.key);
     closeModal();
   } else if (event.key == "ArrowRight") {
-    // right arrow
+    changeSlide(1);
   } else if (event.key == "ArrowLeft") {
-    // left arrow
+    changeSlide(-1);
   }
 };
 
 const openModal = (e) => {
-  console.log("open modal");
   document.body.style.overflow = "hidden"; // prevents from background scrolling
   // overscroll-behavior: contain may be another option
 
   // Find index of image
   currentSlide = zoomableImages.indexOf(e.target);
+  console.log("Current slide: ", currentSlide);
   console.log("Slide index", zoomableImages.indexOf(e.target));
 
   // Show correct image
-  modalImages[currentSlide].style.display = "inline";
+  modalImages[currentSlide].style.display = "flex";
 
   modal.style.display = "flex";
+  // change slide to flex
 
   // For accessibility & keyboard access
   modal.focus();
@@ -66,13 +67,32 @@ const closeModal = () => {
 // let mainPage = document.getElementById("main-page");
 
 // Change slides
-const changeSlides = (n) => {
-  // if it's negative, go to last slide
-  // if it's positive, go to next slide
+const changeSlide = (n) => {
+  // close last current slide image
+  modalImages[currentSlide].style.display = "none";
+
+  // Change slide
+  if (currentSlide === 0 && n === -1) {
+    // if going back from first slide
+    currentSlide = modalImages.length - 1;
+    console.log("Go to last slide");
+  } else if (currentSlide === modalImages.length - 1 && n === 1) {
+    // if going forward from last slide
+    currentSlide = 0;
+    console.log("Go to 0 slide");
+  } else {
+    currentSlide += n;
+    console.log("Go to slide ", currentSlide);
+  }
+
+  // TODO
+  // Change display according to slide number
+  modalImages[currentSlide].style.display = "flex";
+
+  // show current slide image
 };
 
 // Event Listeners
-
 let zoomableImages = Object.values(document.getElementsByClassName("zoomable")); // Make into an array
 zoomableImages.forEach((image) => {
   image.addEventListener("click", openModal);
@@ -82,6 +102,11 @@ let modal = document.getElementsByClassName("modal")[0];
 modal.addEventListener("keydown", keyboardHandler);
 
 let modalImages = document.getElementsByClassName("slide");
+
+let leftArrow = document.getElementsByClassName("left-arrow");
+let rightArrow = document.getElementsByClassName("right-arrow");
+leftArrow[0].addEventListener("click", () => changeSlide(-1));
+rightArrow[0].addEventListener("click", () => changeSlide(1));
 
 let close = document.getElementsByClassName("close");
 close[0].addEventListener("click", closeModal);
