30ad56247674b1c542407fffedd097c67621e104
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2022-04-16 23:41:48 -0400

    Start setup for trapping focus on modal

--- project.js
+++ project.js
@@ -1,14 +1,15 @@
 // TODO
 // Accessibility
-// Aria hidden for modal start
-// Toggle aria hidden for modal
 // Save last focused item (button clicked that opened modal? event in openmodal?)
 // Toggle focus on close
 // -1 tab index for all previous focusable elements
+// restrain focus
 // Check keyboard focus for all lightbox event triggers (thumbnails)
 // Add aria types to all images
-// Add note about controlling the lightbox (arrow keys and escape)
+// Add screen-reader/off-screen note about controlling the lightbox (arrow keys and escape)
 // Add labels to all images
+
+// ***
 // Thumbnail Alt = View full size version of my screenshot of _____
 // Lightbox Alt = Fuller description of image
 // Meaningful link text / alt for arrow buttons
@@ -24,8 +25,8 @@ const scrollToTop = () => {
 
 const keyboardHandler = (event) => {
   event = event || window.event; // if e is undefined, set to window.event
-  console.log("keyboard triggered");
-  console.log(event.key);
+  // console.log("keyboard triggered");
+  // console.log(event.key);
 
   // only if modal is open
   if (event.key == "Escape") {
@@ -34,6 +35,10 @@ const keyboardHandler = (event) => {
     changeSlide(1);
   } else if (event.key == "ArrowLeft") {
     changeSlide(-1);
+  } else if (event.key == "Tab" && event.shiftKey) {
+    console.log("Shift tab");
+  } else if (event.key == "Tab") {
+    console.log("Just tab");
   }
 };
 
@@ -52,9 +57,16 @@ const openModal = (e) => {
   modal.style.display = "flex";
   // change slide to flex
 
+  trapFocus();
+};
+
+const trapFocus = () => {
   // For accessibility & keyboard access
+  modal.tabIndex = -1; // also: setAttribute("tabindex", "-1");
   modal.focus();
 };
+
+const releaseFocus = () => {};
 
 const closeModal = () => {
   document.body.style.overflow = "auto";
@@ -75,14 +87,11 @@ const changeSlide = (n) => {
   if (currentSlide === 0 && n === -1) {
     // if going back from first slide
     currentSlide = modalImages.length - 1;
-    console.log("Go to last slide");
   } else if (currentSlide === modalImages.length - 1 && n === 1) {
     // if going forward from last slide
     currentSlide = 0;
-    console.log("Go to 0 slide");
   } else {
     currentSlide += n;
-    console.log("Go to slide ", currentSlide);
   }
 
   // TODO
@@ -103,10 +112,10 @@ modal.addEventListener("keydown", keyboardHandler);
 
 let modalImages = document.getElementsByClassName("slide");
 
-let leftArrow = document.getElementsByClassName("left-arrow");
-let rightArrow = document.getElementsByClassName("right-arrow");
-leftArrow[0].addEventListener("click", () => changeSlide(-1));
-rightArrow[0].addEventListener("click", () => changeSlide(1));
+let leftArrow = document.getElementsByClassName("left-arrow")[0];
+let rightArrow = document.getElementsByClassName("right-arrow")[0];
+leftArrow.addEventListener("click", () => changeSlide(-1));
+rightArrow.addEventListener("click", () => changeSlide(1));
 
-let close = document.getElementsByClassName("close");
-close[0].addEventListener("click", closeModal);
+let close = document.getElementsByClassName("close")[0];
+close.addEventListener("click", closeModal);
